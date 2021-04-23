b9a5ba5616401dec2456e3454dd5cca7c742b096
Hu Shihang <its.hushihang@gmail.com>
2021-04-23 21:11:21 +0800

    change some codes

--- libcpu/risc-v/rv32m1/interrupt.c
+++ libcpu/risc-v/rv32m1/interrupt.c
@@ -20,9 +20,9 @@ void SystemIrqHandler(uint32_t mcause)
 {
     uint32_t intNum;
 
-    if (mcause & 0x80000000) /* For external interrupt. */
+    if (mcause & 0x80000000) /* For external interrupt. get the value of the 31st bit*/
     {
-        intNum = mcause & 0x1FUL;
+        intNum = mcause & 0x1FUL; // get the values of the 0th~4th bits
 
         /* Clear pending flag in EVENT unit .*/
         EVENT_UNIT->INTPTPENDCLEAR = (1U << intNum);
